3bf1a4734ab4c0d6824a99645665f94e2854708c
sandeepdj <sandeepdj1988@gmail.com>
2017-12-28 17:54:51 +0530

    local

---
+++ src/app/app.router.ts
@@ -0,0 +1,8 @@
+import {Routes, RouterModule} from "@angular/router";
+
+
+
+const ROUTES: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'search', component: SearchComponent }
+   ];
